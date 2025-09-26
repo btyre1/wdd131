0c144fd70153a0fca4f52e5af030adaade47521a
Bryce Tyre <bryc11499@gmail.com>
2025-09-26 11:29:05 -0600

    Added new section and h2 using js

--- Sandbox/dom-basics.js
+++ Sandbox/dom-basics.js
@@ -11,3 +11,6 @@ const newDiv = document.createElement("div");
 newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
 document.body.appendChild(newDiv);
 
+const newSection = document.createElement("section");
+newSection.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
+document.body.appendChild(newSection);
