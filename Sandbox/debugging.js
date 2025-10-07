cf77f7acbaba1174f355d4d7b4400f0191d1da9a
Bryce Tyre <bryc11499@gmail.com>
2025-10-06 19:09:27 -0600

    Completed Activity 2

--- Sandbox/debugging.js
+++ Sandbox/debugging.js
@@ -1,9 +1,13 @@
-const pi = 3.14;
-let radius = 3;
+const PI = 3.14;
 let area = 0;
-area = radius * radius * pi;
-console.log(area);
 
-radius = 4;
-area = radius * radius * pi;
-console.log(area);
+function circleArea(radius) {
+  const area = radius * radius * PI;
+  return area;
+}
+
+area = circleArea(3);
+console.log("Area1:", area);
+
+area = circleArea(4);
+console.log("Area2:", area);
