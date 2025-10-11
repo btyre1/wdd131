e1fa58ed8cf3377494188dcd0d7148fe28dcbde9
Bryce Tyre <bryc11499@gmail.com>
2025-10-11 15:02:45 -0600

    Finished cool pics part 2

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -18,4 +18,47 @@ function handleResize() {
 
 handleResize();
 window.addEventListener("resize", handleResize);
-    
+
+var gallery = document.querySelector('.gallery');
+
+function handleGalleryClick(event) {
+  var clickedImage = event.target.closest('img');
+
+  if (!clickedImage) {
+    return;
+  }
+
+  var src = clickedImage.getAttribute('src');
+  var alt = clickedImage.getAttribute('alt');
+
+  var largeImage = src.split('-')[0] + '-full.jpeg';
+
+  var modal = document.createElement('dialog');
+
+  var modalImage = document.createElement('img');
+  modalImage.setAttribute('src', largeImage);
+  modalImage.setAttribute('alt', alt);
+
+  var closeButton = document.createElement('button');
+  closeButton.className = 'close-viewer';
+  closeButton.textContent = 'X';
+
+  modal.appendChild(modalImage);
+  modal.appendChild(closeButton);
+
+  document.body.appendChild(modal);
+
+  modal.showModal();
+
+  closeButton.addEventListener('click', () => {
+    modal.close();
+  });
+
+  modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.close();
+    }});
+}
+
+gallery.addEventListener('click', handleGalleryClick);
+
