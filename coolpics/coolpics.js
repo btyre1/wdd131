57a9c5da02bcbda3ce5c9774b6c979ac8e8a3455
Bryce Tyre <bryc11499@gmail.com>
2025-10-11 12:37:48 -0600

    Fixed menu button & menu for all screen sizes

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,8 +1,21 @@
-const button = document.getElementById("#menu-button");
-button.addEventListener("click", loadMenu);
-
-function loadMenu() {
-    const nav = document.querySelector("nav");
-    nav.classList.toggle("hide");
+const menuButton = document.querySelector(".menu-button");
+function toggleMenu() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("hide");
 }
+
+menuButton.addEventListener("click", toggleMenu);
+
+function handleResize() {
+  const menu = document.querySelector(".menu");
+  if (window.innerWidth > 1000) {
+    menu.classList.remove("hide");
+  } 
+  else {
+    menu.classList.add("hide");
+  }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
     
