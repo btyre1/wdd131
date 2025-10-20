e2f93ceb26bc000030c891f7092128be526e10de
Bryce Tyre <bryc11499@gmail.com>
2025-10-19 22:36:50 -0600

    Setup for array practice

---
+++ Sandbox/arrays.js
@@ -0,0 +1,6 @@
+const steps = ["one", "two", "three"];
+function listTemplate(step) {
+  return //the html string made from step
+}
+const stepsHtml = // use map to convert the list from strings to HTML
+document.querySelector("#myList").innerHTML = // set the innerHTML
