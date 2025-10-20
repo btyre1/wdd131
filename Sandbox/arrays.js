90bfdc3cd57601c41103bcfc0cc9ab02f7be87a2
Bryce Tyre <bryc11499@gmail.com>
2025-10-20 10:43:19 -0600

    Completed activity 2

--- Sandbox/arrays.js
+++ Sandbox/arrays.js
@@ -4,3 +4,15 @@ const listTemplate(step) {
 }
 const stepsHtml = steps.map(listTemplate);
 document.querySelector("#myList").innerHTML = stepsHtml.join();
+
+const letterGrades = ['A', 'B', 'A'];
+const convertToGPA(grade) {
+    let points = 0;
+    if (grade === "A") {
+        points = 4;
+    } else if (grade === "B") {
+        points = 3;
+    }
+    return points;
+}
+const gpas = letterGrades.map(convertToGPA);
