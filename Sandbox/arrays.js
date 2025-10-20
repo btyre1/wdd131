df1631bff0db89ceb91835fcb50c2134889fdaa8
Bryce Tyre <bryc11499@gmail.com>
2025-10-20 10:30:41 -0600

    Completed activity 1

--- Sandbox/arrays.js
+++ Sandbox/arrays.js
@@ -1,6 +1,6 @@
 const steps = ["one", "two", "three"];
-function listTemplate(step) {
-  return //the html string made from step
+const listTemplate(step) {
+  return `<li>${step}</li>`;
 }
-const stepsHtml = // use map to convert the list from strings to HTML
-document.querySelector("#myList").innerHTML = // set the innerHTML
+const stepsHtml = steps.map(listTemplate);
+document.querySelector("#myList").innerHTML = stepsHtml.join();
