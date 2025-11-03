c4a4e29bc2f704f1155927c72735f0d766f0d591
Bryce Tyre <bryc11499@gmail.com>
2025-11-03 13:42:10 -0700

    Completed activity 2

--- Sandbox/courses.js
+++ Sandbox/courses.js
@@ -17,28 +17,53 @@ const aCourse = {
       instructor: "Sis A",
     },
   ],
+  enrollStudent: function (sectionNum) {
+    const sectionIndex = this.sections.findIndex(
+      (section) => section.sectionNum == sectionNum
+    );
+    if (sectionIndex >= 0) {
+      this.sections[sectionIndex].enrolled++;
+      renderSections(this.sections);
+    }
+  },
+  dropStudent: function (sectionNum) {
+    const sectionIndex = this.sections.findIndex(
+      (section) => section.sectionNum == sectionNum
+    );
+    if (sectionIndex >= 0) {
+      this.sections[sectionIndex].enrolled--;
+      renderSections(this.sections);
+    }
+  },
 };
 
 function setCourseInfo(course) {
   const courseName = document.querySelector("#courseName");
-  const courseCode = document.querySelector("#courseCode");
+  const coursecode = document.querySelector("#courseCode");
   courseName.textContent = course.name;
-  courseCode.textContent = course.code;
+  coursecode.textContent = course.code;
 }
 
-function sectionTemplate(section) {
-  return `<tr>
+function renderSections(sections) {
+  const html = sections.map(
+    (section) => `<tr>
     <td>${section.sectionNum}</td>
     <td>${section.roomNum}</td>
     <td>${section.enrolled}</td>
     <td>${section.days}</td>
     <td>${section.instructor}</td></tr>`
-}
-
-function renderSections(sections) {
-  const html = sections.map(sectionTemplate);
+  );
   document.querySelector("#sections").innerHTML = html.join("");
 }
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+  const sectionNum = document.querySelector("#sectionNumber").value;
+  aCourse.enrollStudent(sectionNum);
+});
+document.querySelector("#dropStudent").addEventListener("click", function () {
+  const sectionNum = document.querySelector("#sectionNumber").value;
+  aCourse.dropStudent(sectionNum);
+});
 
 setCourseInfo(aCourse);
 renderSections(aCourse.sections);
