4fd1dd31926862985504dc9a0607f7d7a752ac8c
Bryce Tyre <bryc11499@gmail.com>
2025-11-16 18:34:30 -0700

    Completed activity 3

--- Sandbox/sorting.js
+++ Sandbox/sorting.js
@@ -81,11 +81,20 @@ function compareFn(a,b) {
 
 const anotherSort = simpleList.sort(compareFn)
 
-function searchList(list, query) {
-    function searchCallback(string) {
-      return string.toLowerCase().includes(query.toLowerCase());
+function searchList(list, q) {
+    function searchCallback(item) {
+      return (
+        item.name.toLowerCase().includes(q.toLowerCase()) ||
+        item.description.toLowerCase().includes(q.toLowerCase()) ||
+        item.tags.find((tag) => tag.toLowerCase().includes(q.toLowerCase()))
+      );
     }
-    return list.filter(searchCallback);
+    const filtered = list.filter(searchCallback);
+
+    const sorted = filtered.sort((a, b) => a.distance > b.distance);
+    return sorted;
 }
-console.log(searchList(simpleList, "b"));
-console.log(searchList(simpleList, "an"));
+
+console.log(searchList(hikes, "yellowstone"));
+console.log(searchList(hikes, "moderate"));
+console.log(searchList(hikes, "al"));
